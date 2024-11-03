a1c7fc31420213965263416b87ec415120656c88
Waleed Zaryab <westendcrew1982@gmail.com>
2024-11-03 18:55:09 +1100

    updated server code

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,26 @@
-import { Router } from 'express';
-const router = Router();
+import dotenv from 'dotenv';
+import express from 'express';
+import path from 'path';
+import weatherRouter from './routes/api/weatherRoutes'; // path to weatherRoutes
 
-import apiRoutes from './api/index.js';
-import htmlRoutes from './htmlRoutes.js';
+dotenv.config();
 
-router.use('/api', apiRoutes);
-router.use('/', htmlRoutes);
+const app = express();
+const PORT = process.env.PORT || 3001;
 
-export default router;
+// Serve static files from the client dist folder
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Use the weather router with the base path
+app.use('/api/weather', weatherRouter);
+
+// Catch-all route to serve index.html for any unmatched routes (for client-side routing)
+app.get('*', (_req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+// Start the server
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`))
